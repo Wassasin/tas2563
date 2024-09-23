c76f44ab454910e951e715346efa36bf983c5629
Wouter Geraedts <git@woutergeraedts.nl>
2024-09-23 16:32:04 +0200

    Added command line arguments to cfgtransform

--- util/cfgtransform/src/main.rs
+++ util/cfgtransform/src/main.rs
@@ -1,24 +1,47 @@
 pub mod ast;
 pub mod bulk;
 
-use std::io::{BufWriter, Write};
+use std::{
+    io::{BufWriter, Write},
+    path::PathBuf,
+};
 
+use clap::Parser;
 use lalrpop_util::lalrpop_mod;
 
 lalrpop_mod!(grammar);
 
+#[derive(Parser, Debug)]
+struct Args {
+    /// Input file (of the cfg format)
+    input: PathBuf,
+
+    /// Output file (of the byte buffer format)
+    #[arg(default_value = "./out.bulk")]
+    output: PathBuf,
+}
+
 fn main() {
     pretty_env_logger::init();
 
-    let f = std::fs::read_to_string("example/test.cfg").unwrap();
+    let args = Args::parse();
+
+    log::info!("Reading {:?}", args.input);
+    let f = std::fs::read_to_string(&args.input).unwrap();
 
     let cmds = grammar::CommandsParser::new().parse(&f).unwrap();
+    log::info!("Read {} commands", cmds.0.len());
 
-    let mut f = BufWriter::new(std::fs::File::create("example/test.bulk").unwrap());
+    let mut f = BufWriter::new(std::fs::File::create(&args.output).unwrap());
+
+    let mut i = 0usize;
     for b in bulk::BulkGenerator::generate(cmds.into_iter()) {
         f.write(&[b]).unwrap();
+        i += 1;
     }
     f.flush().unwrap();
+
+    log::info!("Spooled {} bytes to {:?}", i, args.output);
 }
 
 #[cfg(test)]
